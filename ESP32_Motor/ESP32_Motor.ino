809ffa68468a19f35e283a7e77f07e756c0857fb
Ziller <44690800+33895465@users.noreply.github.com>
2023-02-24 11:59:31 +0800

    2/24
    更新

--- ESP32_Motor/ESP32_Motor.ino
+++ ESP32_Motor/ESP32_Motor.ino
@@ -1,12 +1,15 @@
-// Left_motorctrl pin define
+#include <ros.h>
+
+
+/* Left_motorctrl pin define */
 #define PWM1 27
 #define INA1 26
 #define INB1 25
-// Right_motorctrl pin define
+/* Right_motorctrl pin define */
 #define PWM2 13
 #define INA2 12
 #define INB2 14
-// Encoder pin define
+/* Encoder pin define */
 #define L_ENC_A 35
 #define L_ENC_B 34
 #define R_ENC_A 23
@@ -21,6 +24,8 @@ hw_timer_t * timer_L = NULL;
 
 /* 宣告任務變數 */
 TaskHandle_t Task_Motorcontrol;
+
+ros::NodeHandle nh;
 
 volatile long Encoder_R, Encoder_L;
 double LeftMotor_RPM;
@@ -96,10 +101,7 @@ void READ_ENC_L(){
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  setCpuFrequencyMhz(240);
-
+void Controller_Pinsetup(){
   pinMode(PWM1, OUTPUT);
   pinMode(INA1, OUTPUT);
   pinMode(INB1, OUTPUT);
@@ -110,7 +112,6 @@ void setup() {
   pinMode(L_ENC_B, INPUT);
   pinMode(R_ENC_A, INPUT);
   pinMode(L_ENC_B, INPUT);
-  vTaskDelay(200);
 
   digitalWrite(INA1, HIGH);
   digitalWrite(INB1, HIGH);
@@ -118,20 +119,30 @@ void setup() {
   digitalWrite(INA2, HIGH);
   digitalWrite(INB2, HIGH);
   digitalWrite(PWM2, LOW);
-  vTaskDelay(100);
 
   ledcSetup(0, 5000, 8);
   ledcSetup(1, 5000, 8);
   ledcAttachPin(PWM1, 0);
   ledcAttachPin(PWM2, 1);
 
+}
+
+void setup() {
+  Serial.begin(115200);
+  setCpuFrequencyMhz(240);
+
+  Controller_Pinsetup();
+
+  nh.initNode();
+
   xTaskCreatePinnedToCore(Motor_control, "Motor Control", 10000, NULL, 1, &Task_Motorcontrol, 1);
   vTaskDelay(500);
 
   attachInterrupt(R_ENC_A, READ_ENC_R, CHANGE);
-  timer_R = timerBegin(0, 80, true);
+  timer_R = timerBegin(0, 80, true);                  // timer為80MHz的頻率，因此需要分頻成1MHz，計時器的step為1us(1*10^-6Hz)
+  
   timerAttachInterrupt(timer_R, &R_EncoderISR, true);
-  timerAlarmWrite(timer_R, 500000, true);
+  timerAlarmWrite(timer_R, 500000, true);             // 500000 = 500 ms
   timerAlarmEnable(timer_R);
 
   attachInterrupt(L_ENC_A, READ_ENC_L, CHANGE);
@@ -142,9 +153,9 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Right motor:");
-  Serial.println(RightMotor_RPM);
-  Serial.print("Left motor:");
-  Serial.println(LeftMotor_RPM);
+  // Serial.print("Right motor:");
+  // Serial.println(RightMotor_RPM);
+  // Serial.print("Left motor:");
+  // Serial.println(LeftMotor_RPM);
   vTaskDelay(200);
 }
